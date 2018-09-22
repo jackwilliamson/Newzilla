6e5f9f0df6a9d51391cb6706313126cb5ce5ff55
William Gill <gill.william@gmail.com>
2018-09-22 17:23:45 -0400

    created the headline component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Headlines from './Headlines'
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="container">
+          <Headlines linkText={"Insert Headline here"} linkImg={false}/>
+        </div>
       </div>
     );
   }
