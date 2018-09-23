a6a85a523d83d2b52fd6b8e46a53c958469e58da
Jack Williamson <jack.williamson@sympatico.ca>
2018-09-22 22:28:19 -0400

    fetch calls

--- frontend/src/Grid.js
+++ frontend/src/Grid.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Grid extends Component {
 
-  constructor(props) {
-    super(props)
+  componentDidMount() {
+    axios.get('http://192.168.1.126:8081/topics').then(res => {
+      const newsArticles = res.data
+      this.setState({newsArticles})
+      console.log(this.state)
+      console.log(res)
+    })
   }
 
   renderRow(row) {
@@ -29,9 +35,9 @@ class Grid extends Component {
   render() {
     return (
       <div className="container">
-      {this.props.row.map(
-        (row) => {
-          return (this.renderRow(row))
+      {this.state.newsArticles.map(
+        (article) => {
+          return (this.renderRow(article))
         }
       )}
       </div>
@@ -40,7 +46,31 @@ class Grid extends Component {
 }
 
 
-
-
+// class FetchDemo extends React.Component {
+//   state = {
+//     posts: []
+//   }
+//
+//   componentDidMount() {
+//     axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+//       .then(res => {
+//         const posts = res.data.data.children.map(obj => obj.data);
+//         this.setState({ posts });
+//       });
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>{`/r/${this.props.subreddit}`}</h1>
+//         <ul>
+//           {this.state.posts.map(post =>
+//             <li key={post.id}>{post.title}</li>
+//           )}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
 export default Grid;
