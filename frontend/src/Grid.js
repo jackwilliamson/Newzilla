d4e787d8c0215592848d07aa6ff0647d84151db5
Jack Williamson <jack.williamson@sympatico.ca>
2018-09-22 23:35:35 -0400

    display data

--- frontend/src/Grid.js
+++ frontend/src/Grid.js
@@ -8,28 +8,37 @@ var myStyle = {
 
 var center = {
   marginTop: '125px',
+  width: '200px',
 }
 
 
 class Grid extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      loaded: false,
+    };
+  }
+
   componentDidMount() {
     axios.get('http://192.168.1.126:8081/topics').then(res => {
       const newsArticles = res.data
-      this.setState({newsArticles})
-      console.log(this.state)
-      console.log(res)
+      this.setState({
+        newsArticles,
+        loaded: true
+      })
     })
   }
 
-  renderRow(row, index) {
+  renderRow(articles, row) {
     return (
       <div className="row">
-      <p style={center}>Topics</p>
-      {row.col.map(
-        (col) => {
+      <p style={center}>{articles.topic}</p>
+      {Array.isArray(articles.sources) && articles.sources.slice(0,4).map(
+        (source, index) => {
           return (
-            this.renderCol(index)
+            this.renderCol(source, row, index)
           )
         }
       )}
@@ -37,13 +46,11 @@ class Grid extends Component {
     )
   }
 
-  renderCol(index) {
+  renderCol(source, row, col) {
     return (
       <div className="col-sm">
-          {index == 0 &&
-            <p style={myStyle}>Sources</p>
-          }
-        <Headlines linkText={"Insert Headline here"} linkImg={"https://d262ilb51hltx0.cloudfront.net/max/800/1*uTAT1bayI6ek6esb-BcZhg.jpeg"}/>
+          <p style={myStyle}>{source.source_name}</p>
+          <Headlines linkText={source.title} linkImg={source.image}/>
       </div>
     )
   }
@@ -51,7 +58,7 @@ class Grid extends Component {
   render() {
     return (
       <div className="container">
-      {this.state.newsArticles.map(
+      {this.state.loaded && this.state.newsArticles.map(
         (article, index) => {
           return (this.renderRow(article, index))
         }
@@ -61,33 +68,6 @@ class Grid extends Component {
   }
 }
 
-
-// class FetchDemo extends React.Component {
-//   state = {
-//     posts: []
-//   }
-//
-//   componentDidMount() {
-//     axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
-//       .then(res => {
-//         const posts = res.data.data.children.map(obj => obj.data);
-//         this.setState({ posts });
-//       });
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <h1>{`/r/${this.props.subreddit}`}</h1>
-//         <ul>
-//           {this.state.posts.map(post =>
-//             <li key={post.id}>{post.title}</li>
-//           )}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
 
 
 export default Grid;
